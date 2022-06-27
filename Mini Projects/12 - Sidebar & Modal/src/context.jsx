72b2b1e2e62106282e8d419ed02efe6e9930e8df
Petrovici Catalin-Cristian <petrovicicatalin24@yahoo.com>
2022-06-27 18:10:46 +0300

    React - Mini Projects - Sidebar & Modal - with useContext

---
+++ Mini Projects/12 - Sidebar & Modal/src/context.jsx
@@ -0,0 +1,44 @@
+import React, { useState, useContext } from 'react';
+
+const AppContext = React.createContext();
+
+const AppProvider = ({ children }) => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const openSidebar = () => {
+    setIsSidebarOpen(true);
+  };
+  const closeSidebar = () => {
+    setIsSidebarOpen(false);
+  };
+  const openModal = () => {
+    console.log('open modal');
+    setIsModalOpen(true);
+  };
+  const closeModal = () => {
+    console.log('close modal');
+    setIsModalOpen(false);
+  };
+  return (
+    <AppContext.Provider
+      value={{
+        isModalOpen,
+        isSidebarOpen,
+        openModal,
+        closeModal,
+        openSidebar,
+        closeSidebar,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+// custom hook
+export const useGlobalContext = () => {
+  return useContext(AppContext);
+};
+
+export { AppContext, AppProvider };

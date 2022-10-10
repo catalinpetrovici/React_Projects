3bd76b3ed39fe0fff420a6c2b1f7ab6a23efd4eb
Petrovici Catalin-Cristian <petrovicicatalin24@yahoo.com>
2022-10-10 20:27:41 +0300

    React Query - Pre-populating data, Pre-fetch to pre-populate cache

---
+++ React Query/React Query in Larger App/client/src/components/staff/hooks/useStaff.ts
@@ -0,0 +1,29 @@
+import { Dispatch, SetStateAction, useState } from 'react';
+
+import type { Staff } from '../../../../../shared/types';
+import { axiosInstance } from '../../../axiosInstance';
+import { queryKeys } from '../../../react-query/constants';
+import { filterByTreatment } from '../utils';
+import { useQuery } from 'react-query';
+
+// for when we need a query function for useQuery
+async function getStaff(): Promise<Staff[]> {
+  const { data } = await axiosInstance.get('/staff');
+  return data;
+}
+
+interface UseStaff {
+  staff: Staff[];
+  filter: string;
+  setFilter: Dispatch<SetStateAction<string>>;
+}
+
+export function useStaff(): UseStaff {
+  // for filtering staff by treatment
+  const [filter, setFilter] = useState('all');
+
+  const fallback = [];
+  const { data = fallback } = useQuery(queryKeys.treatments, getStaff);
+
+  return { staff: data, filter, setFilter };
+}
